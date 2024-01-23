a3bf8e56d469c1d03e8b632ebdb86189080141dd
Joseph Angelo <angeloj@sunypoly.edu>
2024-01-23 12:20:29 -0500

    initial setup

---
+++ Assignments/assign_1/assign_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string alphabet = "abc";
+    int state_cnt = 4;
+    int start = 1;
+    bool final[] = {false, true, false, true, false};
+    int cur = start;
+    int trans[state_cnt + 1][alphabet.length()];
+
+    return 0;
+}
