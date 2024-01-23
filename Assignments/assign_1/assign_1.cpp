a415b79ece3aece32efb03935dadcc4c3533a245
Joseph Angelo <angeloj@sunypoly.edu>
2024-01-23 12:40:16 -0500

    first working model (hard coded)

--- Assignments/assign_1/assign_1.cpp
+++ Assignments/assign_1/assign_1.cpp
@@ -9,7 +9,23 @@ int main()
     int start = 1;
     bool final[] = {false, true, false, true, false};
     int cur = start;
-    int trans[state_cnt + 1][alphabet.length()];
+    int trans[5][3] = {{0,0,0}, {2,0,0}, {0,3,2}, {1,0,4}, {3,1,3}};
+    char *s = "abaab";
+
+    while (*s != '\0' && cur != 0)
+    {
+        cur = trans[cur][alphabet.find(*s)];
+        s++;
+    }
+
+    if (final[cur])
+    {
+        cout << "\n\ngood\n\n";
+    }
+    else
+    {
+        cout << "\n\nbad\n\n";
+    }
 
     return 0;
 }
