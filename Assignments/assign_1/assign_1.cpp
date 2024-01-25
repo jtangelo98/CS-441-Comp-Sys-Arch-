87926c8987fb5ec28ea71d120a37e89070e9eec9
Joseph Angelo <angeloj@sunypoly.edu>
2024-01-25 13:36:36 -0500

    current prog 1/25

--- Assignments/assign_1/assign_1.cpp
+++ Assignments/assign_1/assign_1.cpp
@@ -1,16 +1,60 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    string alphabet = "abc";
-    int state_cnt = 4;
-    int start = 1;
-    bool final[] = {false, true, false, true, false};
-    int cur = start;
-    int trans[5][3] = {{0,0,0}, {2,0,0}, {0,3,2}, {1,0,4}, {3,1,3}};
-    char *s = "abaab";
+    string alphabet;
+    int state_cnt;
+    int start;
+    bool final[] = {false, false, false, false, false};
+    int cur;
+    char *s;
+
+    //check to see if file was passed to program using command line
+    if(argc < 2)
+    {
+        cerr << "Must provide filename when calling program";
+    }
+ 
+    //file has been passed, retrieve and open file
+    const char* path = argv[1];
+    ifstream inFile(path);
+
+    //check if file could be opened
+    if(!inFile.is_open())
+    {
+        cerr << "Could not open file";
+    }
+
+    //read content of dfa file into variables
+    while(inFile.peek() != EOF)
+    {
+        state_cnt << inFile.get() + 1;
+        start << inFile.get();
+        while(inFile.peek() != '\n')
+        {
+            final[inFile.get()] = true;
+        }
+        while(inFile.peek() != '\n')
+        {
+           alphabet += inFile.get();
+        }
+        vector <vector<int>> trans;
+
+        for(int i = 0; i < state_cnt + 1; i++)
+        {
+            for (int j = 0; j < alphabet.length(); j++)
+            {
+                trans[i][j] << inFile.get();
+            }
+            
+        }
+    }
+
+
 
     while (*s != '\0' && cur != 0)
     {
