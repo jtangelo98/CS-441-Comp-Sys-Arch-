350ad9b0347a307735ce112d86a401c98b86e818
Joseph Angelo <angeloj@sunypoly.edu>
2024-04-23 20:56:19 -0400

    4/23 progress on prog4

--- Assignments/prog_4/angeloj_prog4.cpp
+++ Assignments/prog_4/angeloj_prog4.cpp
@@ -9,19 +9,15 @@ typedef struct {
     bool dirty = false;
     unsigned int LRU = 0;
     uint64_t tag;
-
 } block;
 
 int main(int argc, char* argv[]){
     if (argc < 5) {
-        cerr << "Error: input should be the following ... <address_file> <sets_pow> <bps_pow> <wpb_pow>" << endl;
+        cerr << "Error: input should be the following ... <executable> <address_file> <sets_pow> <bps_pow> <wpb_pow>" << endl;
         return 1;
     }
-
     //open address file
     ifstream f(argv[1]);
-
-    
 
     //cache parameters (set count, blocks per set, number of adresses per block.)
     int sets_pow = stoi(argv[2]);
@@ -31,53 +27,51 @@ int main(int argc, char* argv[]){
     int wpb_pow = stoi(argv[4]);
     int wpb = 1 << wpb_pow;
     int set_num;
-    int tag;
+    uint64_t tag;
     int total_accesses= 0;
     int num_hits = 0;
     int largest_lru = 0;
     string line;
     uint64_t addr;
     vector <block> cache(sets * bps);
-// if(f.is_open()){
-//     getline(f, line);
-//         cout << line;
-//         return 1;
-//     }
-    while(f.peek()!= EOF){
-        f >> hex >> addr;
-        addr >>= wpb_pow;
+    cout << cache.size(); return 0;
+
+    while(f >> hex >> addr){
+        addr >>= wpb_pow; 
         set_num = addr & (~ (~ 0U << sets_pow));
         tag = addr >> sets_pow;
-
-        for(int i = 0; i < bps; i++)
-        {
-            
-            if(!cache[(set_num * bps) + i].dirty){
-                cache[(set_num * bps) + i].LRU ++;
-                if(tag == cache[(set_num * bps) + i].tag){
+        
+        //multiply set number by blocks per set to get index into the cache
+        set_num = (set_num * bps);
+        int index_of_LRU = set_num;
+        largest_lru = cache[set_num].LRU;
+        for(int i = 0; i < bps; i++){
+            cache[set_num + i].LRU++;
+            if(!cache[set_num + i].dirty){
+                cache[set_num + i].tag = tag;
+                cache[set_num + i].dirty = true;
+                cache[(set_num + i)].LRU = 0;
+                break;
+            }
+            else{
+                if(tag == cache[set_num + i].tag){
                     num_hits++;
-                    cache[(set_num * bps) + i].LRU = 0;
+                    cache[(set_num + i)].LRU = 0;
                     break;
                 }
             }
-            
-            if(i == (bps - 1)){
-                for(int j = 0; j < bps - 1; j++){
-                    if(cache[(set_num * bps) + (j + 1)].LRU > cache[(set_num * bps) + j].LRU){
-                        largest_lru = (set_num * bps) + (j + 1);
-                    }
-                    else{
-                         largest_lru = (set_num * bps) + j;
-                    }
-                }
-                cache[largest_lru].tag = tag;
+            if(largest_lru < cache[set_num + i].LRU){
+                largest_lru = cache[set_num + i].LRU;
+                index_of_LRU = set_num + i;
             }
-            total_accesses++;
+            if(i == (bps - 1)){
+                cache[index_of_LRU].tag = tag;
+                cache[index_of_LRU].LRU = 0;
+            }
         }
 
+        total_accesses++;
     }
-
-    cout << "hits: " << num_hits << "/" << total_accesses;;
-
+    cout << "hits: " << num_hits << "/" << total_accesses;
     return 0;
 }
